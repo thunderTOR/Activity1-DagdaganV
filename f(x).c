3b9c269b2d5ffc1539d855f24ee6a51b12dd5215
thunderTOR <victor_dagdagan@dlsu.edu.ph>
2016-05-26 17:12:50 +0800

    Create f(x).c

---
+++ f(x).c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+   int X;
+   float C;
+   printf("Enter the Value of X : ");
+   scanf("%d", &X);
+   
+   C = X/70;
+   printf ("the value of X is %f",C);
+}
+   
