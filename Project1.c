d0d563a08baca8c5cb55218ea351c28687fc16e2
thunderTOR <victor_dagdagan@dlsu.edu.ph>
2016-05-26 17:08:19 +0800

    Create Project1.c

---
+++ Project1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+printf("\t\t@@@@@@@@@@@@@@@@@@@@@@@\n");
+printf("\t\tVictor M. Dagdagan\n");
+printf("\t\tCivil Engineering\n");
+printf("\t\t11148861\n");
+printf("\t\tDecember 9, 1994\n");
+printf("\t\tvictor_dagdagan@dlsu.edu.ph\n");
+printf("\t\t09175240244\n");
+printf("\t\t@@@@@@@@@@@@@@@@@@@@@@@\n");
+
+}
