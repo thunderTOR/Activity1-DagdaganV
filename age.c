cf881312fec68f1856d520718a1e901980beb741
thunderTOR <victor_dagdagan@dlsu.edu.ph>
2016-05-26 17:11:23 +0800

    Create age.c

---
+++ age.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+   int yr;
+   float n;
+   printf("Enter Birth year : ");
+   scanf("%d", &yr);
+   
+   n = 2016 - yr;
+   printf ("the appropriate age is %f",n);
+}
+   
